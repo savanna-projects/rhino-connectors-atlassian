999c20fe395232f035b6ab8156b621b0bf8fe4d2
Roei Sabag <gravity.api@outlook.com>
2022-03-21 12:12:42 +0200

    resolve #153

---
+++ src/Rhino.Connectors.AtlassianClients/Contracts/JiraIssue.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Rhino.Connectors.AtlassianClients.Contracts
+{
+    [DataContract]
+    internal class JiraIssue
+    {
+        [DataMember]
+        public JiraFields Fields { get; set; }
+
+        [DataContract]
+        internal class JiraFields
+        {
+            [DataMember]
+            public Project Project { get; set; }
+
+            [DataMember]
+            public string Summary { get; set; }
+
+            [DataMember]
+            public string Description { get; set; }
+
+            [DataMember]
+            public string Environment { get; set; }
+
+            [DataMember, JsonPropertyName("issuetype")]
+            public IssueType IssueType { get; set; }
+
+            [DataMember]
+            public Priority Priority { get; set; }
+        }
+
+        [DataContract]
+        internal class Project
+        {
+            [DataMember]
+            public string Key { get; set; }
+        }
+
+        [DataContract]
+        internal class IssueType
+        {
+            [DataMember]
+            public string Name { get; set; }
+        }
+
+        [DataContract]
+        internal class Priority
+        {
+            [DataMember]
+            public string Id { get; set; }
+        }
+    }
+}
